3ffa535c64c60a589b70a63c7bbc2e2d95f1fcf1
renatoviegas <renato.viegas@hotmail.com>
2017-09-03 08:48:41 -0300

    Finalização do módulo 07

---
+++ js/remove-paciente.js
@@ -0,0 +1,9 @@
+var tblPaciente = document.querySelector("#tabela-pacientes");
+
+tblPaciente.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    } ,500);    
+});
